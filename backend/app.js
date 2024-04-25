4c695f3110c2b4af8ab630aff7ec66aef8c56d1c
Antoine-NVE <antoinenavette@gmail.com>
2024-04-25 20:03:57 +0200

    Enregistrez et récupérez des données

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
+const Thing = require('./models/Thing');
+
 mongoose
     .connect(
-        'mongodb+srv://antoinenavette:VwKhgG3GfgaYRHDv@testtask.rs1gspo.mongodb.net/?retryWrites=true&w=majority&appName=testtask'
+        process.env.MONGO_URL
         // { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -27,35 +30,26 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet créé',
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body, // Equivaut à écrire tous les champs 1 par 1
     });
+    thing
+        .save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch((error) => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then((things) => res.status(200).json(things))
+        .catch((error) => res.status(404).json({ error }));
 });
 
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 1,
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl:
-                'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 1,
-        },
-        {
-            _id: 2,
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl:
-                'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 1,
-        },
-    ];
-
-    res.status(200).json(stuff);
+    Thing.find()
+        .then((things) => res.status(200).json(things))
+        .catch((error) => res.status(400).json({ error }));
 });
 
 module.exports = app;
