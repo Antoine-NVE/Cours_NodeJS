71c5cbef7868386887f93da33d09eeaafc923676
Antoine-NVE <antoinenavette@gmail.com>
2024-04-25 19:05:51 +0200

    Configurez votre base de données

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose
+    .connect(
+        'mongodb+srv://antoinenavette:VwKhgG3GfgaYRHDv@testtask.rs1gspo.mongodb.net/?retryWrites=true&w=majority&appName=testtask'
+        // { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
