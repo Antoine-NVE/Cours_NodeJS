e45427c987f5b9a7f34797e7793e56234b88e592
Antoine-NVE <antoinenavette@gmail.com>
2024-04-28 09:31:54 +0200

    Créez des tokens d'authentification

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -33,7 +34,11 @@ exports.login = (req, res, next) => {
                         if (valid) {
                             res.status(200).json({
                                 userId: user._id,
-                                token: 'TOKEN',
+                                token: jwt.sign(
+                                    { userId: user._id },
+                                    'RANDOM_TOKEN_SECRET',
+                                    { expiresIn: '24h' }
+                                ),
                             });
                         } else {
                             res.status(401).json({
